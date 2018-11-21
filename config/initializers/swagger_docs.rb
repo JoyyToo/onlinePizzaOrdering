6f103e8fb464a0fc0e3ea99ae0631400d09e92f4
joyytoo <joytoo97@gmail.com>
2018-11-21 20:30:13 +0300

    Add base path for heroku

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -4,7 +4,11 @@ Swagger::Docs::Config.register_apis('1.0' => {
                                       # the output location where your .json files are written to
                                       api_file_path: 'public',
                                       # the URL base path to your API
-                                      base_path: 'http://localhost:3000',
+                                      base_path: if Rails.env.development?
+                                                   'http://localhost:3000'
+                                                 else
+                                                   'https://thawing-oasis-83740.herokuapp.com'
+                                                 end,
 
                                       # if you want to delete all .json files at each generation
                                       clean_directory: true,
