2f8945badc765e704d2c803a7f4876844c28e47c
joyytoo <joytoo97@gmail.com>
2018-11-27 20:18:08 +0300

    Fix base path

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -4,7 +4,7 @@ Swagger::Docs::Config.register_apis('1.0' => {
                                       # the output location where your .json files are written to
                                       api_file_path: 'public',
                                       # the URL base path to your API
-                                      base_path: 'https://thawing-oasis-83740.herokuapp.com',
+                                      base_path: ENV['ROOT_URL'],
 
                                       # if you want to delete all .json files at each generation
                                       clean_directory: true,
