67e05122cbb90706a379396eecac6ea1891780c8
joyytoo <joytoo97@gmail.com>
2019-01-21 15:08:18 +0300

    Capture account activation using headers

--- app/controllers/concerns/activate_account.rb
+++ app/controllers/concerns/activate_account.rb
@@ -1,9 +1,13 @@
 module ActivateAccount
 
   def account_activated
-    token = AuthToken.decode(request.headers['Authorization'].split(' ').last)
-    email = token.first.values[1]
-    user = User.find_by(email: email).id
+    if request.headers['Authorization']
+      token = AuthToken.decode(request.headers['Authorization'].split(' ').last)
+      email = token.first.values[1]
+      user = User.find_by(email: email).id
+    else
+      user = User.find_by(email: params[:email]).id
+    end
     this_user = User.find(user)
 
     if this_user.activated?
